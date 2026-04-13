03a3ee278abb1f4489bbc11ede8005d6b07b9165
Alvin11David <alvin69david@gmail.com>
2026-04-13 15:27:09 +0300

    Auto commit at Mon, Apr 13, 2026  3:27:08 PM

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
@@ -25,5 +25,9 @@ export const analytics =
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+  connectFirestoreEmulator(db, "localhost", 8080);
+}
 
 export default app;
